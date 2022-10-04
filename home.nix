ee7caa5938a63736d426a00335ba518349ecbc76
wnxkiv85 <8f2w79@getgoogleoff.me>
2022-10-03 22:38:36 -0300

    zsh

--- home.nix
+++ home.nix
@@ -54,7 +54,14 @@ in
 				enable = true;
 				theme = "alanpeabody";
 			};
-			
+	initExtraFirst = ''
+		# Base16 Shell
+		BASE16_SHELL="$HOME/.config/base16-shell-master/"
+		[ -n "$PS1" ] && \
+		    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+		        source "$BASE16_SHELL/profile_helper.sh"
+      '';		
+      
 		};
 	programs.git = {
 			enable = true;
@@ -80,6 +87,13 @@ in
 				sha512 = "1l4l9i0v1n4c4p9047wywhcwr4kdir1lrl34pi1fjbswgvsjyzwmjrxqy1zjihz3prpck040mdxaimqcmhzn9icz93221ids6rvvk33";
 						};
 				target = ".local/share/icons/Sakuya-cursors";
+			};
+		"base16-shell-master" = {
+				source = pkgs.fetchzip {
+				url = "https://github.com/chriskempson/base16-shell/archive/refs/heads/master.zip" ;
+				sha512 = "0c0lm47wwcqd2lc0293awr8kvpib8pcq1kgnilnbg0d2g0b65if0aj0yrmv1sg3z78c15ssksv9j7ikbhhwifdg3360ypk2plrfyrny";	
+				};
+				target = ".config/base16-shell-master";
 			};
 		};
 		dconf.settings = with lib.hm.gvariant; {
