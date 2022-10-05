ab7790231e8c7313fc9a737dc109a4d00682b741
wnxkiv85 <8f2w79@getgoogleoff.me>
2022-10-05 13:42:37 -0300

    dkn

--- syspackages.nix
+++ syspackages.nix
@@ -4,14 +4,12 @@
 	virtualisation.podman = {
 		enable = true;
 		dockerCompat = true;	
-	};
-	
+	};	
 	environment.systemPackages = with pkgs; [ 
 		micro
 		rnnoise-plugin
 		nerdfonts
 	];
-
 	environment.gnome.excludePackages = (with pkgs; [
 		gnome-photos
 		gnome-tour
